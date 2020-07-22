d0d21db4b25ef436c6d28ea9556cf3500b024324
avivmachrus <avivmachrus@gmail.com>
2020-07-22 17:06:09 +0700

    styling and layouting messenger

--- src/App.js
+++ src/App.js
@@ -45,10 +45,14 @@ function App() {
 
   return (
     <div className="App">
+      <img
+        src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"
+        alt="fb-messenger-logo"
+      />
       <h1>Hello Newbee</h1>
       <h2>Welcome {username}</h2>
 
-      <form>
+      <form className="app_form">
         <FormControl>
           {/* input field */}
           <InputLabel>Enter a Message...</InputLabel>
